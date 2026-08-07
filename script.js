4204f416ccc0f630733abc336542de4b1ddcd17b
mbabaev1 <qfast@internet.ru>
2026-08-08 02:57:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ burger.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
 
+document.querySelectorAll(".menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
+});
+
 const links = document.querySelectorAll(".menu a");
 
 links.forEach(link => {
@@ -12,3 +18,4 @@ links.forEach(link => {
         menu.classList.remove("active");
     });
 });
+
