53c5314dc9701161aae8770b3990e14013fac0cf
mbabaev1 <qfast@internet.ru>
2026-08-13 00:47:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ document.querySelectorAll(".menu a").forEach(link => {
         burger.classList.remove("open");
     });
 });
+
+document.addEventListener("click", (event) => {
+    const clickInsideMenu = menu.contains(event.target);
+    const clickOnBurger = burger.contains(event.target);
+
+    if (
+        menu.classList.contains("active") &&
+        !clickInsideMenu &&
+        !clickOnBurger
+    ) {
+        menu.classList.remove("active");
+        burger.classList.remove("open");
+    }
+});
